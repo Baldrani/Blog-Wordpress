5eb5a30b3646f54b195e62dc09911eb1b1de1508
douael <elomaridouae@gmail.com>
2017-02-03 15:04:12 +0100

    Mise en place du slider dernieres annonces de chat

--- wp-content/themes/3IW1-groupe1/index.php
+++ wp-content/themes/3IW1-groupe1/index.php
@@ -1,49 +1,18 @@
 <?php
 get_header();
 
-if (have_posts()){ ?>
-    <?php
-    $args = array( 'numberposts' => '4', 'post_status' => 'publish' );
-
-    $recent_posts = wp_get_recent_posts( $args );
-
-    ?>
-    <section id="slideshow">
-    <div class="container">
-        <div class="c_slider"></div>
-        <div class="slider">
-            <?php
-
-            //$args = array( 'numberposts' => '4', 'post_status' => 'publish' );
-
-            $i=1;
-
-
-            foreach( $recent_posts as $recent ){
-
-                if ($i <= 4) {
-
-                    //$toto = get_the_date('Y-m-d', $recent["ID"]);
-                    the_post(); ?><a href="<?php echo the_permalink(); ?>" title="Aller à l'annonce"><figure><?php the_post_thumbnail( array(640,310) ); ?><figcaption><?php the_title(); ?></figcaption></figure></a>
-                    <?php
-                }
-                $i++;
-
-            }
-
-
-
-            ?>
-        </div>
-    </div>
-    <span id="timeline"></span>
-    </section><?php
+if (have_posts()){
+  while (have_posts()) {
+    the_post();?>
+    <h2><?php the_title(); ?></h2>
+    <?php the_content();
+  }
 }
 else {
-    echo 'Pas d\'article';
+  echo 'Pas d\'article';
 }
 
 dynamic_sidebar('sidebar-1');
 
 get_footer();
-?>
+ ?>
