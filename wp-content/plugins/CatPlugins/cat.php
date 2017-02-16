2e9a3df0ed1e9206b90f5fd00448e6a52c1b8051
Mayon Maël <mael.mayon@free.fr>
2017-02-16 18:07:12 +0100

    J'ai failli craquer

--- wp-content/plugins/CatPlugins/cat.php
+++ wp-content/plugins/CatPlugins/cat.php
@@ -2,11 +2,12 @@
 /*
 Plugin Name: Cat Widgets
 Plugin URI: google.com
-Description: Un plugin sur les chats
+Description: Des plugins sur les chats
 Version: 0.1
 Author: Maël Mayon
 Author URI: maelmayon.fr
 License: GPL2
 */
 
-require_once('class/Cat_Plugin.class.php');
+require_once('class/Cat_Plugins.class.php');
+new Cat_Plugins();
--- wp-content/plugins/CatPlugins/widgets/Jqvmap_France_Widget.class.php
+++ wp-content/plugins/CatPlugins/widgets/Jqvmap_France_Widget.class.php
@@ -3,7 +3,7 @@ class Jqvmap_France_Widget extends WP_Widget
 {
     public function __construct()
     {
-        parent::__construct('zero_newsletter', 'Newsletter', array('description' => 'Un formulaire d\'inscription à la newsletter.'));
+        parent::__construct('jqvmap_france', 'Carte de France', array('description' => 'Dispose une carte de France avec les chats disponibles.'));
     }
 
     public function widget($args, $instance)
